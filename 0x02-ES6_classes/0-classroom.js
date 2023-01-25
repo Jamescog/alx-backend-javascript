d9a399af92b1f246b74e5f548e0dccd7a5ae2068
Jamescog <jamescog72@gmail.com>
2023-01-26 00:31:47 +0300

    fix

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,12 +1,4 @@
-/**
- * Represents a class room.
- */
 export default class ClassRoom {
-  /**
-   * Creates a new  ClassRoom.
-   * @param {Number} maxStudentsSize - The maximum number of
-   * students in the class.
-   */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
