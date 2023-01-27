8c90c408ab107115bc1f112052c235aeeab6551d
Jamescog <jamescog72@gmail.com>
2023-01-27 20:11:19 +0300

    the function is needed

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,8 +1,11 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const currentYear = new Date().getFullYear();
   return {
-    [`income-${currentYear}`]: income,
-    [`gdp-${currentYear}`]: gdp,
-    [`capita-${currentYear}`]: capita,
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
   };
 }
