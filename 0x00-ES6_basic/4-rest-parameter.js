54a430001aa4d1db02265ce96561e0addaeead6c
Jamescog <jamescog72@gmail.com>
2023-01-27 20:08:40 +0300

    error fixed by directly returing the object itself rather than creating object

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,9 +1,8 @@
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const currentYear = new Date().getFullYear();
-  const budget = {
+  return {
     [`income-${currentYear}`]: income,
     [`gdp-${currentYear}`]: gdp,
     [`capita-${currentYear}`]: capita,
   };
-  return budget;
 }
