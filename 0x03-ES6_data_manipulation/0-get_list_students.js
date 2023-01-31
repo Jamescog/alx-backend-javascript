b3d97e6b3669b6b5096ea986355782153f684595
Jamescog <jamescog72@gmail.com>
2023-01-31 20:00:05 +0300

    initial commit

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+function getListStudents() {
+  const arrayOfStudents = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return arrayOfStudents;
+}
+
+export default getListStudents;
