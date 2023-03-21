5f3db3cc700aa090f21fb0e05281a04de513c757
Jamescog <jamescog72@gmail.com>
2023-03-21 19:28:32 +0300

    this file contains the function that prints argument to stdout, it should run on node as running environment

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * prints in STDOUT the string argument
+ * @param {str} s -  the string argument
+ * @return {null} - return nothing
+ */
+
+const displayMessage = (s) => {
+  console.log(s)
+};
+
+module.exports = displayMessage;
