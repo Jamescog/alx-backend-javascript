bc3cf56e4fd856f700c4848de779d8e2c02575b8
Jamescog <jamescog72@gmail.com>
2023-04-14 15:27:03 +0300

    Error Unexpected space before function parentheses  space-before-function-paren is fixed

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,10 @@
-export default function getResponseFromAPI() {
+export default function getResponseFromAPI () {
   const num = Math.random() * 10;
   return new Promise((resolve, reject) => {
     if (num > 5) {
-      resolve("promise is kept");
+      resolve('promise is kept');
     } else {
-      reject(new Error("I can't"));
+      reject(new Error('I cannot'));
     }
   });
 }
