aa6f7d1bde73a8c520d460c691b3cfed9834da65
Jamescog <jamescog72@gmail.com>
2023-04-14 14:34:42 +0300

    add solution for task 9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+// this function takes one argument and create new array called queue
+const guardrail = (mathFunction) => {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.message);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+};
+export default guardrail;
