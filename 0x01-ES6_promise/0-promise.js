9199ef3ab19240f4228884cc6561002241a41aed
Jamescog <jamescog72@gmail.com>
2023-04-14 15:15:07 +0300

    task zero with kept promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-function getResponseFromAPI(response, resolve) {
+export default function getResponseFromAPI () {
   return new Promise((resolve, reject) => {
-    const concept = "Promise"
-    resolve(response);
-    console.log(concept)
-    reject(error);
-    console.log("Promise is not kept")
+    resolve('promise is kept');
   });
 }
-=======
-export default function getResponseFromAPI() {
-  return new Promise(() => {});
-}
->>>>>>> a38e745d5e29b025b3949178aa9361e3980de26d
