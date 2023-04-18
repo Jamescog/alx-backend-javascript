4a070b7570fd594d2ae022314eaeea537d6c98df
Jamescog <jamescog72@gmail.com>
2023-04-18 10:29:56 +0300

    new class with getter and setter

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,16 @@
 export default class ClassRoom {
-  constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize;
+  constructor (maxStudentsSize) {
+    this._maxStudentSize = maxStudentsSize;
+  }
+
+  set maxStudentsSize (value) {
+    if (typeof value !== 'number') {
+      throw new Error('Size of the student must be type of Number');
+    }
+    this._maxStudentSize = value;
+  }
+
+  get maxStudentsSize () {
+    return this._maxStudentSize;
   }
 }
