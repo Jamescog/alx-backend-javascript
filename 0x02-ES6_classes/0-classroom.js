c103807c4708277567bdc27a47b8634e5f0509bc
Jamescog <jamescog72@gmail.com>
2023-04-18 10:44:23 +0300

    fix the getter and setter so that it can be accessed

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,16 +1,5 @@
 export default class ClassRoom {
-  constructor (maxStudentsSize) {
-    this._maxStudentSize = maxStudentsSize;
-  }
-
-  set maxStudentsSize (value) {
-    if (typeof value !== 'number') {
-      throw new Error('Size of the student must be type of Number');
-    }
-    this._maxStudentSize = value;
-  }
-
-  get maxStudentsSize () {
-    return this._maxStudentSize;
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
